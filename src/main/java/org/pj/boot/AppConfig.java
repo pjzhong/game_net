adc5900916d61f60929fbda4bdad6d275de578a0
pj_zhong <pj_zhong@163.com>
2020-05-26 11:36:05 +0800

    Mysql-ConfigDao配置

--- src/main/java/org/pj/boot/AppConfig.java
+++ src/main/java/org/pj/boot/AppConfig.java
@@ -1,6 +1,10 @@
 package org.pj.boot;
 
 import com.mongodb.ConnectionString;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -8,10 +12,26 @@ import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDbFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 @PropertySource("classpath:/server.properties")
 public class AppConfig {
+
+  @Bean(value = "configDataSource", destroyMethod = "close")
+  public HikariDataSource configDataSource(Environment env) {
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl(env.getRequiredProperty("db.config.url"));
+    config.setDriverClassName("com.mysql.jdbc.Driver");
+    config.setMaximumPoolSize(50);
+    config.setConnectionInitSql("select 1");
+    return new HikariDataSource(config);
+  }
+
+  @Bean("configDao")
+  public JdbcTemplate configJdbcTemplate(@Qualifier("configDataSource") DataSource dataSource) {
+    return new JdbcTemplate(dataSource);
+  }
 
   @Bean(destroyMethod = "closeClient")
   public SimpleMongoClientDbFactory simpleMongoClientDatabaseFactory(Environment env) {
