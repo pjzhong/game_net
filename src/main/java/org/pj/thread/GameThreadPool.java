52e79560c0731759add9805cafd69d09515e3215
pj_zhong <pj_zhong@163.com>
2020-06-05 19:38:25 +0800

    GameThreadPool使用

--- src/main/java/org/pj/thread/GameThreadPool.java
+++ src/main/java/org/pj/thread/GameThreadPool.java
@@ -48,7 +48,7 @@ public class GameThreadPool {
    * @since 2020年04月02日 20:51:49
    */
   public ExecutorService getPool(Object o) {
-    int h = o.hashCode();
+    int h = System.identityHashCode(o.hashCode());
     int idx = (h ^ (h >>> 16)) & limit;
     hashStat.merge(idx, 1L, Long::sum);
     return pools[idx];
