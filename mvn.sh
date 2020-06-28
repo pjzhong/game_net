fe0d94f3a2c3201eaacc2f7455464cac85bca826
pj_zhong <pj_zhong@163.com>
2020-06-28 10:42:28 +0800

    docker image adjusted

--- mvn.sh
+++ mvn.sh
@@ -8,5 +8,5 @@ docker run -it \
            --network game_network \
            -v $(pwd):/project \
            -v /opt/repository/maven:/root/.m2/repository \
-           ping:maven-3.6-jdk-11-aliyun mvn $PARAMS
+           pjzhong/maven-3.6-jdk-11-aliyun mvn $PARAMS
 
